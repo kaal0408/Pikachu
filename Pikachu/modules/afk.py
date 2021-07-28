fc8f457a89e239d860675fd714dfe76d6eb88365
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-07-28 13:39:23 +0530

    Update afk.py

--- Pikachu/modules/afk.py
+++ Pikachu/modules/afk.py
@@ -1,12 +1,12 @@
 import random, html
 
-from KristinaBot import dispatcher
-from KristinaBot.modules.disable import (
+from Pikachu import dispatcher
+from Pikachu.modules.disable import (
     DisableAbleCommandHandler,
     DisableAbleMessageHandler,
 )
-from KristinaBot.modules.sql import afk_sql as sql
-from KristinaBot.modules.users import get_user_id
+from Pikachu.modules.sql import afk_sql as sql
+from Pikachu.modules.users import get_user_id
 from telegram import MessageEntity, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, Filters, MessageHandler, run_async
