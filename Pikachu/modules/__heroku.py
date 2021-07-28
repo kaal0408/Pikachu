3bd8cb90354ddf9f591c0167c08f98cd3cbd79cb
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-07-28 13:24:13 +0530

    Update __heroku.py

--- Pikachu/modules/__heroku.py
+++ Pikachu/modules/__heroku.py
@@ -5,8 +5,8 @@ import os
 import heroku3
 import requests
 
-from KristinaBot import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
-from KristinaBot.events import register
+from Pikachu import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
+from Pikachu.events import register
 
 heroku_api = "https://api.heroku.com"
 Heroku = heroku3.from_key(HEROKU_API_KEY)
