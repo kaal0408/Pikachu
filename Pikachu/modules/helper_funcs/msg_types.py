1f51b9eb906050b379ec986fd16ef89a2f739fe3
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-07-28 12:16:59 +0530

    Update msg_types.py

--- Pikachu/modules/helper_funcs/msg_types.py
+++ Pikachu/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from KristinaBot.modules.helper_funcs.string_handling import button_markdown_parser
+from Pikachu.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
