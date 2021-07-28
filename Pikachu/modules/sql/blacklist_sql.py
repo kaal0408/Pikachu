63c6adb7fc92cdbb10bd4daffc80b99f77f33d53
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-07-28 12:22:26 +0530

    Update blacklist_sql.py

--- Pikachu/modules/sql/blacklist_sql.py
+++ Pikachu/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from KristinaBot.modules.sql import SESSION, BASE
+from Pikachu.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
