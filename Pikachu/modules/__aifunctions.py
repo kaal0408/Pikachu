0f9c8aa386f38f8befce92600d384c20680e56b6
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-07-28 12:43:47 +0530

    Update __aifunctions.py

--- Pikachu/modules/__aifunctions.py
+++ Pikachu/modules/__aifunctions.py
@@ -1,7 +1,7 @@
 import requests
 url = "https://iamai.p.rapidapi.com/ask"
-from KristinaBot import telethn, OWNER_ID
-from KristinaBot.events import register
+from Pikachu import telethn, OWNER_ID
+from Pikachu.events import register
 from telethon import events
 from telethon import types
 from telethon.tl import functions
