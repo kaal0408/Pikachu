fdf77b9a097bb3e69b79431dce9d8ce02f2a3c02
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-07-29 21:29:33 +0530

    Update connection.py

--- Pikachu/modules/connection.py
+++ Pikachu/modules/connection.py
@@ -5,10 +5,10 @@ from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Upda
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CommandHandler, CallbackQueryHandler, run_async
 
-import KristinaBot.modules.sql.connection_sql as sql
-from KristinaBot import dispatcher, DRAGONS, DEV_USERS
-from KristinaBot.modules.helper_funcs import chat_status
-from KristinaBot.modules.helper_funcs.alternate import send_message, typing_action
+import Pikachu.modules.sql.connection_sql as sql
+from Pikachu import dispatcher, DRAGONS, DEV_USERS
+from Pikachu.modules.helper_funcs import chat_status
+from Pikachu.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
