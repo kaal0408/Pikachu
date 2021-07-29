881d04100de241429e70fa4a5131380887ae52ee
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-07-29 21:21:28 +0530

    Update bans.py

--- Pikachu/modules/bans.py
+++ Pikachu/modules/bans.py
@@ -5,7 +5,7 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from KristinaBot import (
+from Pikachu import (
     DEV_USERS,
     LOGGER,
     OWNER_ID,
@@ -15,8 +15,8 @@ from KristinaBot import (
     WOLVES,
     dispatcher,
 )
-from KristinaBot.modules.disable import DisableAbleCommandHandler
-from KristinaBot.modules.helper_funcs.chat_status import (
+from Pikachu.modules.disable import DisableAbleCommandHandler
+from Pikachu.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
@@ -27,9 +27,9 @@ from KristinaBot.modules.helper_funcs.chat_status import (
     user_can_ban,
     can_delete,
 )
-from KristinaBot.modules.helper_funcs.extraction import extract_user_and_text
-from KristinaBot.modules.helper_funcs.string_handling import extract_time
-from KristinaBot.modules.log_channel import gloggable, loggable
+from Pikachu.modules.helper_funcs.extraction import extract_user_and_text
+from Pikachu.modules.helper_funcs.string_handling import extract_time
+from Pikachu.modules.log_channel import gloggable, loggable
 
 
 @run_async
