6d1e21c5e74617e2892ae2ab74d78293bbd4fbf4
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-07-29 21:30:02 +0530

    Update cricketscore.py

--- Pikachu/modules/cricketscore.py
+++ Pikachu/modules/cricketscore.py
@@ -5,7 +5,7 @@ import urllib.request
 
 from bs4 import BeautifulSoup
 from telethon import events
-from KristinaBot import telethn as tbot
+from Pikachu import telethn as tbot
 from telethon.tl import functions, types
 from telethon.tl.types import *
 
