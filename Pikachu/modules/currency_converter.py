f7fa5cc31ec0e53bfa4b4746b6374fb08ed02fe1
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-07-30 17:47:09 +0530

    Update currency_converter.py

--- Pikachu/modules/currency_converter.py
+++ Pikachu/modules/currency_converter.py
@@ -1,5 +1,5 @@
 import requests
-from KristinaBot import CASH_API_KEY, dispatcher
+from Pikachu import CASH_API_KEY, dispatcher
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
