6de98d97ec8b21b3e3847f3fa84c0ac96687508f
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-08-03 00:00:52 +0530

    Update wiki.py

--- Pikachu/modules/wiki.py
+++ Pikachu/modules/wiki.py
@@ -1,6 +1,6 @@
 import wikipedia
-from KristinaBot import dispatcher
-from KristinaBot.modules.disable import DisableAbleCommandHandler
+from Pikachu import dispatcher
+from Pikachu.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
