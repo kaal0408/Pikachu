184d4536147f3418baf82cf9853287a59f08150f
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-08-02 23:47:16 +0530

    Update stickers.py

--- Pikachu/modules/stickers.py
+++ Pikachu/modules/stickers.py
@@ -12,8 +12,8 @@ from telegram import TelegramError, Update
 from telegram.ext import run_async, CallbackContext
 from telegram.utils.helpers import mention_html
 
-from KristinaBot import dispatcher
-from KristinaBot.modules.disable import DisableAbleCommandHandler
+from Pikachu import dispatcher
+from Pikachu.modules.disable import DisableAbleCommandHandler
 
 combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
