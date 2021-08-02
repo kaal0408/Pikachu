250160c44ec23f0df65a20da2bb6466f91178701
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-08-02 13:22:29 +0530

    Update gtranslator.py

--- Pikachu/modules/gtranslator.py
+++ Pikachu/modules/gtranslator.py
@@ -3,8 +3,8 @@ from google_trans_new import LANGUAGES, google_translator
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
-from KristinaBot import dispatcher
-from KristinaBot.modules.disable import DisableAbleCommandHandler
+from Pikachu import dispatcher
+from Pikachu.modules.disable import DisableAbleCommandHandler
 
 
 @run_async
