818746433f9d7df22878b52a8c33e4a6b08d80b5
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-08-02 16:21:22 +0530

    Update log_channel.py

--- Pikachu/modules/log_channel.py
+++ Pikachu/modules/log_channel.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 from telegram.ext import CallbackContext
 
-from KristinaBot.modules.helper_funcs.misc import is_module_loaded
+from Pikachu.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
@@ -13,9 +13,9 @@ if is_module_loaded(FILENAME):
     from telegram.ext import CommandHandler, JobQueue, run_async
     from telegram.utils.helpers import escape_markdown
 
-    from KristinaBot import EVENT_LOGS, LOGGER, dispatcher
-    from KristinaBot.modules.helper_funcs.chat_status import user_admin
-    from KristinaBot.modules.sql import log_channel_sql as sql
+    from Pikachu import EVENT_LOGS, LOGGER, dispatcher
+    from Pikachu.modules.helper_funcs.chat_status import user_admin
+    from Pikachu.modules.sql import log_channel_sql as sql
 
     def loggable(func):
         @wraps(func)
