6c0560b1d32495e77dd9f120166fd6525ba399c6
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-08-29 16:36:13 +0530

    Update dev.py

--- Pikachu/modules/dev.py
+++ Pikachu/modules/dev.py
@@ -18,7 +18,7 @@ from telegram.ext import CallbackContext, CommandHandler, run_async
 def allow_groups(update: Update, context: CallbackContext):
     args = context.args
     if not args:
-        update.effective_message.reply_text(f"Current state: {KristinaBot.ALLOW_CHATS}")
+        update.effective_message.reply_text(f"Current state: {Pikachu.ALLOW_CHATS}")
         return
     if args[0].lower() in ["off", "no"]:
         Pikachu.ALLOW_CHATS = True
