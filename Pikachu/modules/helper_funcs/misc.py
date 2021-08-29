df95e744849a4e2085a407e9130cb105866393fc
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-08-29 16:23:11 +0530

    Update misc.py

--- Pikachu/modules/helper_funcs/misc.py
+++ Pikachu/modules/helper_funcs/misc.py
@@ -67,11 +67,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("👈🏻", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("💝", callback_data="kristina_back"),
+                EqInlineKeyboardButton("💝", callback_data="Pikachu_back"),
              EqInlineKeyboardButton("👉🏻", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="kristina_back")]]
+        pairs += [[EqInlineKeyboardButton("Back", callback_data="Pikachu_back")]]
 
     return pairs
 
