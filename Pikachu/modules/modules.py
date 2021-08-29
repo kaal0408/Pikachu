0e8f6c78787753cdf90c2f6489661b613ec64862
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-08-29 16:26:09 +0530

    Update modules.py

--- Pikachu/modules/modules.py
+++ Pikachu/modules/modules.py
@@ -98,7 +98,7 @@ def unload(update: Update, context: CallbackContext):
     )
 
     try:
-        imported_module = importlib.import_module("KristinaBot.modules." + text)
+        imported_module = importlib.import_module("Pikachu.modules." + text)
     except:
         unload_messasge.edit_text("Does that module even exist?")
         return
