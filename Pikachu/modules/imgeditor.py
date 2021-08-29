9912e4791760a6c2833f1107632d37579c8200cb
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-08-29 16:24:24 +0530

    Update imgeditor.py

--- Pikachu/modules/imgeditor.py
+++ Pikachu/modules/imgeditor.py
@@ -10,7 +10,7 @@ from pyrogram.types import (
 )
 
 # By @TroJanzHEX
-from KristinaBot.ImageEditor.edit_1 import (  # pylint:disable=import-error
+from Pikachu.ImageEditor.edit_1 import (  # pylint:disable=import-error
     black_white,
     box_blur,
     bright,
