7cd6d019745d028b5d4f932f6ba0409b913af2ae
∂ιρєѕн <83916289+Itzdipesh@users.noreply.github.com>
2021-09-21 18:26:49 +0530

    Update quotely.py

--- Pikachu/modules/quotely.py
+++ Pikachu/modules/quotely.py
@@ -13,7 +13,7 @@ import random
 import json
 import os
 import re
-from Pikachu.Pikachu import Pikachu
+from Pikachu import Pikachu
 
 COLORS = [
     "#F07975", "#F49F69", "#F9C84A", "#8CC56E", "#6CC7DC", "#80C1FA", "#BCB3F9", "#E181AC"]
