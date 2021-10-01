eb753d0a5ac14b45ee018b60b7da0990a0e08668
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-10-01 12:29:34 +0530

    Update dbfunc.py

--- Pikachu/utils/dbfunc.py
+++ Pikachu/utils/dbfunc.py
@@ -1,7 +1,8 @@
 from Pikachu.mongo import db
 from typing import Dict, List, Union
 from Pikachu.utils.dbfunc import (update_karma, get_karma, get_karmas,
-from Pikachu.utils.dbfunc import get_couple, save_couple
+from Pikachu.utils.dbfunc import (get_couple, save_couple,
+
 coupledb =>  db.couple
 
 
