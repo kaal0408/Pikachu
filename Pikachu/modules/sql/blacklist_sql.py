5dc494e6e652d559d34a2e1bf310c0d2dee6f067
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-10-03 10:15:35 +0530

    Update blacklist_sql.py

--- Pikachu/modules/sql/blacklist_sql.py
+++ Pikachu/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, Integer
 
-from AsunaRobot.modules.sql import SESSION, BASE
+from Pikachu.modules.sql import SESSION, BASE
 
 
 class BlackListFilters(BASE):
