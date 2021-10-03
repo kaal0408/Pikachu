3e56d957f0eeb4aa677f8b4ffc5ffe0a7b2be168
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-10-03 10:23:14 +0530

    Update reporting_sql.py

--- Pikachu/modules/sql/reporting_sql.py
+++ Pikachu/modules/sql/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from AsunaRobot.modules.sql import BASE, SESSION
+from Pikachu.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String
 
 
