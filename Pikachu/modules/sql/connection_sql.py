c07b5177032b6fdcc906ca95132127675cce786b
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-10-03 10:17:11 +0530

    Update connection_sql.py

--- Pikachu/modules/sql/connection_sql.py
+++ Pikachu/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 
-from AsunaRobot.modules.sql import SESSION, BASE
+from Pikachu.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
