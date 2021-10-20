5d744a70e3e4a55b38c5abb549f4f6eff2c54b22
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-10-20 12:10:15 +0530

    Update speed_test.py

--- Pikachu/modules/speed_test.py
+++ Pikachu/modules/speed_test.py
@@ -1,5 +1,6 @@
 import speedtest
 from Pikachu import DEV_USERS, dispatcher
+from Pikachu.modules.disable import DisableAbleCommandHandler
 from Pikachu.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
