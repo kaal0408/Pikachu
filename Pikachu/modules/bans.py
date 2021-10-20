2f06f23c13e21f298fa73d70281bd9346bd09f0a
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-10-20 12:21:35 +0530

    Update bans.py

--- Pikachu/modules/bans.py
+++ Pikachu/modules/bans.py
@@ -405,7 +405,6 @@ __help__ = """
  ❍ /sban <userhandle>*:* Silently ban a user. Deletes command, Replied message and doesn't reply. (via handle, or reply)
  ❍ /tban <userhandle> x(m/h/d)*:* bans a user for `x` time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
  ❍ /unban <userhandle>*:* unbans a user. (via handle, or reply)
- ❍ /punch <userhandle>*:* Punches a user out of the group, (via handle, or reply)
 
  *Admins only:*
  ❍ /mute <userhandle>*:* silences a user. Can also be used as a reply, muting the replied to user.
