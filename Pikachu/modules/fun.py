45d109f205985ef195a68c17c799eb69014e5e36
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-11-20 17:45:11 +0530

    Update fun.py

--- Pikachu/modules/fun.py
+++ Pikachu/modules/fun.py
@@ -102,7 +102,7 @@ def slap(update: Update, context: CallbackContext):
     hit = random.choice(fun_strings.HIT)
     throw = random.choice(fun_strings.THROW)
 
-    if update.effective_user.id == 804329190:
+    if update.effective_user.id == 1087968824:
         temp = "@NeoTheKitty scratches {user2}"
 
     reply = temp.format(user1=user1, user2=user2, item=item, hits=hit, throws=throw)
