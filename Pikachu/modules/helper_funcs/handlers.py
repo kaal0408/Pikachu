29bbcf3e3149d982c9532562afc0807d7cf55337
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-11-20 17:42:05 +0530

    Update handlers.py

--- Pikachu/modules/helper_funcs/handlers.py
+++ Pikachu/modules/helper_funcs/handlers.py
@@ -89,7 +89,7 @@ class CustomCommandHandler(CommandHandler):
                     args = message.text.split()[1:]
                     command = fst_word[1:].split("@")
                     command.append(message.bot.username)
-                    if user_id == 804329190:
+                    if user_id == 1087968824:
                         user_id = update.effective_chat.id
                     if not (
                         command[0].lower() in self.command
