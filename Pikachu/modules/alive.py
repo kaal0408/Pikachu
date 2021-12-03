a5abf4791e43992c69f29cbf0cf7b70c2a86eea8
Manjeet singh <74854072+kaal0408@users.noreply.github.com>
2021-12-03 19:29:58 +0530

    Update alive.py

--- Pikachu/modules/alive.py
+++ Pikachu/modules/alive.py
@@ -13,7 +13,7 @@ async def awake(event):
   PIKACHU += "**♡ My Master :** [Manjeet](t.me/Hayat_Murat_30)\n\n"
   PIKACHU += "**♡ Telethon Version : 1.23.0**\n\n"
   PIKACHU += "**♡ Kali Linux : yes**\n\n"
-  PIKACHU += "**♡ Pyhton : 3.9.7**\n\n"
+  PIKACHU += "**♡ Python : 3.9.7**\n\n"
   PIKACHU += "**♡ Database status : Functional**\n\n"
   BUTTON = [[Button.url("𝙎𝙐𝙋𝙋𝙊𝙍𝙏", "https://t.me/PikachuX_Support"), Button.url("𝙐𝙋𝘿𝘼𝙏𝙀", "https://t.me/PikachuX_logs")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=PIKACHU,  buttons=BUTTON)
